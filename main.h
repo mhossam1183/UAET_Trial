9ad9f7df9e7945f29363340382b61a75de3c4ec2
Mohamed Hossam <mohamed.hossam.1183@gmail.com>
2022-10-21 04:59:05 +0200

    stable

--- main.h
+++ main.h
@@ -19,6 +19,8 @@ typedef unsigned long int      uint32_t;
 #define FORCE_BIT_FIELD_VALUE(arg, value)          arg = value
 #define CLEAR_BIT_FIELD(arg)                       arg &= 0x00000000
 
+/*-----------------------------------------------------------------------------------------------*/
+
 /* General-Purpose Input/Output Run Mode Clock Gating Control */
 typedef struct
 {
@@ -41,6 +43,8 @@ typedef union
 #define SYSTEM_CTRL_RCGCGPIO_OFFSET     0x608
 
 #define SYSTEM_CTRL_RCGCGPIO_R          ( * ( (volatile RCGCGPIO_Tag *) (SYSTEM_CTRL_BASE_ADDRESS + SYSTEM_CTRL_RCGCGPIO_OFFSET) ) )
+
+/*-----------------------------------------------------------------------------------------------*/
 
 /* GPIO PORTF base address */
 #define GPIO_PORTF_BASE_ADDRESS             0x40025000
@@ -99,5 +103,65 @@ typedef union
 #define GPIO_PORTF_GPIODEN_OFFSET     0x51C
 
 #define GPIO_PORTF_GPIODEN_R          ( * ( (volatile GPIODEN_Tag *) (GPIO_PORTF_BASE_ADDRESS + GPIO_PORTF_GPIODEN_OFFSET) ) )
+
+/*-----------------------------------------------------------------------------------------------*/
+
+/* Core Peripherals base address */
+#define CORE_PPH_BASE_ADDRESS             0xE000E000
+
+/* SysTick Control Register */
+typedef struct
+{
+	uint32_t ENABLE        :1;
+	uint32_t INTEN         :1;
+	uint32_t CLK_SRC       :1;
+	uint32_t Reserved      :13;
+	uint32_t COUNT         :1;
+	uint32_t Reserved_2      :15;
+}STCTRL_BF;
+
+typedef union
+{
+	uint32_t     R;
+	STCTRL_BF  B;
+}STCTRL_Tag;
+
+#define CORE_PPH_STCTRL_OFFSET     0x010
+
+#define CORE_PPH_STCTRL_R          ( * ( (volatile STCTRL_Tag *) (CORE_PPH_BASE_ADDRESS + CORE_PPH_STCTRL_OFFSET) ) )
+
+/* SysTick Reload Value Register */
+typedef struct
+{
+	uint32_t RELOAD        :24;
+	uint32_t Reserved      :8;
+}STRELOAD_BF;
+
+typedef union
+{
+	uint32_t     R;
+	STRELOAD_BF  B;
+}STRELOAD_Tag;
+
+#define CORE_PPH_STRELOAD_OFFSET     0x014
+
+#define CORE_PPH_STRELOAD_R          ( * ( (volatile STRELOAD_Tag *) (CORE_PPH_BASE_ADDRESS + CORE_PPH_STRELOAD_OFFSET) ) )
+
+/* SysTick Current Value Register */
+typedef struct
+{
+	uint32_t CURRENT        :24;
+	uint32_t Reserved      :8;
+}STCURRENT_BF;
+
+typedef union
+{
+	uint32_t     R;
+	STCURRENT_BF  B;
+}STCURRENT_Tag;
+
+#define CORE_PPH_STCURRENT_OFFSET     0x018
+
+#define CORE_PPH_STCURRENT_R          ( * ( (volatile STCURRENT_Tag *) (CORE_PPH_BASE_ADDRESS + CORE_PPH_STCURRENT_OFFSET) ) )
 
 #endif  /* MAIN_H */
