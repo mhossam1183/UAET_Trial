44dd9a41bde840f5e56ad87e4de184951a18a090
Mohamed Hossam <mohamed.hossam.1183@gmail.com>
2022-10-23 03:35:42 +0200

    stable

--- main.c
+++ main.c
@@ -3,39 +3,22 @@
 #include "./MCAL/GPIO.h"
 #include "./MCAL/SysCtrl.h"
 #include "./MCAL/SysTick.h"
-#include "./MCAL/Timer.h"
+#include "./MCAL/Interrupt.h"
 
-#define Vectors_Size 155
-
-extern void Function_1(void);
-
-void Function_1(void)
-{
-	GPIO_PORTF_GPIODATA_R.B.DATA ^= GPIO_PF3_ENABLE;
-}
-
-static uint32_t Vector_Table_RAM[1] __attribute__((aligned(1024)));
-
-static uint32_t * var1 = 0;
-
-static uint32_t * var2 = 0;
+extern void SysTick_User_Activity(void);
 
 int main(void)
 {
-	var1 = (uint32_t *)(CORE_PPH_VTABLE_R.R + 0x003C);
-	
-	CORE_PPH_VTABLE_R.R = (uint32_t)&Vector_Table_RAM;
-	
-	var2 = (uint32_t *)(CORE_PPH_VTABLE_R.R + 0x003C);
-	
-	*var2 = &Function_1;
-	
 	SET_BIT(SYSTEM_CTRL_RCGCGPIO_R.B.PORTF_CLK);
 	
 	SET_BIT_FIELD_VALUE(GPIO_PORTF_GPIODEN_R.B.DEN, GPIO_PF3_ENABLE);
 	
 	SET_BIT_FIELD_VALUE(GPIO_PORTF_GPIODIR_R.B.DIR, GPIO_PF3_ENABLE);
 	
+	
+	Init_RAM_Vector_Table();
+	
+	Set_SysTick_CallBack(SysTick_User_Activity);
 	
 	SET_BIT_FIELD_VALUE(CORE_PPH_STCURRENT_R.B.CURRENT, 0x0);
 	
@@ -52,4 +35,9 @@ int main(void)
 		{
 			//SET_BIT_FIELD_VALUE(GPIO_PORTF_GPIODATA_R.B.DATA, GPIO_PF3_ENABLE);
 		}
+}
+
+void SysTick_User_Activity(void)
+{
+	GPIO_PORTF_GPIODATA_R.B.DATA ^= GPIO_PF3_ENABLE;
 }
