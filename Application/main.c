33d8cfaf878fffa108c695c747742577c5597453
Mohamed Hossam <mohamed.hossam.1183@gmail.com>
2022-10-25 00:59:47 +0200

    stable

--- Application/main.c
+++ Application/main.c
@@ -1,9 +1,14 @@
+#include "../Libraries/STD_types.h"
 #include "../MCAL/Interrupt.h"
 #include "../MCAL/Timer.h"
 #include "../HAL/Led.h"
 
 /* user function to be used as callback, for defining Timer exception handler */
 extern void TIMER1A_User_Activity(void);
+
+static uint32_t user_cycle_Sec = 2;
+
+static uint32_t cycle_count = 0;
 
 int main(void)
 {
@@ -29,8 +34,13 @@ int main(void)
 /* user function definition */
 void TIMER1A_User_Activity(void)
 {
+	if((cycle_count % (user_cycle_Sec * 2)) == 0)
+	{
 	/* Green LED toggle */
 	Led_Toggle();
+	}
+	
+	cycle_count++;
 	
 	/* clear GPTM "Timer 1 A" Interrupt flag */
 	Timer_Clear_Timer1A_Flag();
